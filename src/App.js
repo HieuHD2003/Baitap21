e09e8ed3854fe9b282f01ac8e6b1454882de05b5
HieuHD2003 <115697606+HieuHD2003@users.noreply.github.com>
2023-05-09 08:20:15 +0700

    21

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import "./style.css";
+import useBooksContext from "./hooks/book-context-hook";
 
-function App() {
+const App = () => {
+  const { fetchBooks } = useBooksContext();
+  useEffect(() => {
+    fetchBooks();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BookList />
+      <BookCreate />
     </div>
   );
-}
+};
 
 export default App;
